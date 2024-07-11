987ee4cd96af3c6c8f7d5684feb52f471934333e
Jelle Meeus <sogladev@gmail.com>
2024-07-11 16:08:29 +0200

    add loader and spell script

--- src/DemonicPactClassic_loader.cpp
+++ src/DemonicPactClassic_loader.cpp
@@ -1,15 +1,18 @@
 /*
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
  */
+#include "Config.h"
 
 // From SC
-void AddMyPlayerScripts();
+void AddSC_demonic_pact_classic_spell_script();
 
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addskeleton_moduleScripts()
+void Addmod_demonic_pact_classicScripts()
 {
-    AddMyPlayerScripts();
+    if (!(sConfigMgr->GetOption<bool>("DemonicPactClassic.Enable", true)))
+            return;
+    AddSC_demonic_pact_classic_spell_script();
 }
 
