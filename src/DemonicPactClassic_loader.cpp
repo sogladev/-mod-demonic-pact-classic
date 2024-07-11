bec697e34a7aee86f3264dd2a918c8b77b6952c1
Jelle Meeus <sogladev@gmail.com>
2024-07-11 16:23:33 +0200

    update config so it does nothing

--- src/DemonicPactClassic_loader.cpp
+++ src/DemonicPactClassic_loader.cpp
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
  */
-#include "Config.h"
 
 // From SC
 void AddSC_demonic_pact_classic_spell_script();
@@ -11,8 +10,6 @@ void AddSC_demonic_pact_classic_spell_script();
 // additionally replace all '-' in the module folder name with '_' here
 void Addmod_demonic_pact_classicScripts()
 {
-    if (!(sConfigMgr->GetOption<bool>("DemonicPactClassic.Enable", true)))
-            return;
     AddSC_demonic_pact_classic_spell_script();
 }
 
